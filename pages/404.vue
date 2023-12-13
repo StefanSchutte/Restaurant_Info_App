ac7987ae525739cc50d0957576bccc41045d7ebb
Stefan Schutte <2011070287@ufs4life.ac.za>
2023-12-13 20:36:42 +0900

    init

---
+++ pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nav  />
+    <div class="container">
+      <h1>Page Not Found</h1>
+      <a href="/restaurants">Go back to Restaurants </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+
+}
+</style>
+<script setup lang="ts">
+</script>
